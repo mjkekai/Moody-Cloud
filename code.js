46a6e111f1a42e763e90d2f29d75746054a630c7
rileyale001 <alexjtriley@gmail.com>
2021-05-04 20:51:33 -0700

    commiting in develop branch

---
+++ code.js
@@ -0,0 +1,2 @@
+const urlWith = "https://api.weatherstack.com/forecast?access_key=95a73e7c7b18b33bcc43679334309c1d&query=New%20York&forecast_days=3&hourly=1";
+var city = document.querySelector('form input');
