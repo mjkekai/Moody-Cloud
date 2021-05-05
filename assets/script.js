6b098753ff29cabdbf24056005c97a5c0f02bd8e
Matt Koski <mjkekai@gmail.com>
2021-05-04 20:51:26 -0700

    removed the single toggle button

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var sadBtn = document.getElementById('sad').style.display = 'none';
 var hapBtn = document.getElementById('happy').style.display = 'none';
 var angBtn = document.getElementById('angry').style.display = 'none';
-var angBtn = document.getElementById('scared').style.display = 'none';
-var angBtn = document.getElementById('anxious').style.display = 'none';
-var angBtn = document.getElementById('stressed').style.display = 'none';
+var scrdBtn = document.getElementById('scared').style.display = 'none';
+var anxBtn = document.getElementById('anxious').style.display = 'none';
+var strsBtn = document.getElementById('stressed').style.display = 'none';
+
