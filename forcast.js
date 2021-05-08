365f0e4e259a029ef7d3d9dba882953dc0a28217
Alex Gihts <githenyalex@gmail.com>
2021-05-08 00:13:49 -0700

    two days update

---
+++ forcast.js
@@ -0,0 +1,5 @@
+https://api.weatherstack.com/forecast
+    ? access_key = YOUR_ACCESS_KEY
+    & query = New York
+    & forecast_days = 1
+    & hourly = 1
--- script.js
+++ script.js
@@ -21,11 +21,14 @@ var forecastNextDayAfter = $('#forecast-next-day-after')
 // 
 function getWeatherByCity () {
     var city = document.getElementById("city-input").value;
+    var date = document.getElementById('date-forecast');
     $.ajax({
         url: 'https://api.weatherstack.com/forecast',
         data: {
           access_key: access_key,
           query: city,
+          forecast_days: '2'
+
           
 
         },
@@ -38,21 +41,24 @@ function getWeatherByCity () {
 function displayForecast(data) {
     console.log(data); // display data here
     $('#current-weather').append(JSON.stringify(data.current.temperature));
+    $('#forecast-day-2').append(JSON.stringify(data.current.temperature));
+    $('#forecast-day-3').append(JSON.stringify(data.current.temperature));
+     // $('#forecast-day-2').append(JSON.stringify(data.forecast>2021-05-08>date.avetemp));
     
 
 
 
 
 } 
-function displayForecast(data) {
-    console.log(data); // display data here
-    $('#forecast-day-2').append(JSON.stringify(data.forecast.date.avgtemp));
+// function displayForecast(data) {
+//     console.log(data); // display data here
+//     $('#forecast-day-2').append(JSON.stringify(data.forecast.date.avgtemp));
     
 
 
 
 
-} 
+// } 
 searchCityButton.on('click', function(e){
     e.preventDefault();
     getWeatherByCity();
