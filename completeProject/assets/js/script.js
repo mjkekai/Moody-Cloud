f83288a0804756f435c87583151776f73c79c235
Matt Koski <mjkekai@gmail.com>
2021-05-08 01:05:14 -0700

    replaced weather placeholders with weather api

--- completeProject/assets/js/script.js
+++ completeProject/assets/js/script.js
@@ -9,3 +9,21 @@ toggleButton.addEventListener('click', () => {
     /* Toggles between hide() and show() for the selected hamburger menu */
     navigationLinks.classList.toggle('active')
 })
+const access_key = "95a73e7c7b18b33bcc43679334309c1d";
+// stuff
+var cities = {}
+var cityHist = {}
+
+var searchCityInput = $('#city-input')
+var searchCityButton = $('#city-button')
+
+
+var cityListing = $('#city-list')
+var weatherIconCurrent = $('#weather-icon-current')
+var dateDisplay = $('#date-display')
+var temperature = $('#current-temperture')
+
+
+
+var currentWeather = $('#current-weather')
+var forecastNextDay = $('#forecast-next-day')
