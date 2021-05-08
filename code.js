01cc4cd942e134fd4f7f9d7637e15297602b9616
rileyale001 <alexjtriley@gmail.com>
2021-05-07 18:55:16 -0700

    committing on main

--- code.js
+++ code.js
@@ -1,2 +1,45 @@
-const urlWith = "https://api.weatherstack.com/forecast?access_key=95a73e7c7b18b33bcc43679334309c1d&query=New%20York&forecast_days=3&hourly=1";
-var city = document.querySelector('form input');
+// API KEY
+const access_key = "95a73e7c7b18b33bcc43679334309c1d";
+// stuff
+var cities = {}
+var cityHist = {}
+
+var searchCityInput = $('#city-input')
+var searchCityButton = $('#city-button')
+
+
+var cityListing = $('#city-list')
+var weatherIconCurrent = $('#weather-icon-current')
+var dateDisplay = $('#date-display')
+var temperature = $('#current-temperture')
+
+
+
+var currentWeather = $('#current-weather')
+var forecastNextDay = $('#forecast-next-day')
+var forecastNextDayAfter = $('#forecast-next-day-after')
+// 
+function getWeatherByCity () {
+    var city = document.getElementById("city-input").value;
+    $.ajax({
+        url: 'https://api.weatherstack.com/forecast',
+        data: {
+          access_key: access_key,
+          query: city,
+          
+
+        },
+        dataType: 'json',
+        success: function(data) {
+            displayForecast(data)
+        }
+      });
+} 
+function displayForecast(data) {
+    console.log(data); // display data here
+    
+} 
+searchCityButton.on('click', function(e){
+    e.preventDefault();
+    getWeatherByCity();
+});
